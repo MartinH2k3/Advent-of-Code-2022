f99acb266fb9f895ebf60d8a7de9d1f632ca5ac0
MartinH2k3 <martinko.horsky.2003@gmail.com>
2022-12-09 17:17:04 +0100

    Day 9, both parts, better way

--- aoc9.py
+++ aoc9.py
@@ -1,3 +1,13 @@
+def move_tail_closer(head, tail):
+    if abs(head[1] - tail[1]) <= 1 and abs(head[0] - tail[0]) <= 1:
+        return tail
+    tail[0] += tail[0] < head[0]
+    tail[0] -= tail[0] > head[0]
+    tail[1] += tail[1] < head[1]
+    tail[1] -= tail[1] > head[1]
+    return tail
+
+
 def part1():
     with open("input.txt") as file:
         moves = [i.split(" ") for i in file.read().split('\n')]
@@ -5,33 +15,27 @@ def part1():
     tail = [0, 0]
     visited = set()
     for move, value in moves:
-        head[0] += ((move == "U") - (move == "D")) * int(value)
-        head[1] += ((move == "R") - (move == "L")) * int(value)
-        if abs(head[1] - tail[1]) <= 1 and abs(head[0] - tail[0]) <= 1:
-            continue
-
-        if head[0] != tail[0] and head[1] != tail[1]:
-            tail[0] += tail[0] < head[0]
-            tail[0] -= tail[0] > head[0]
-            tail[1] += tail[1] < head[1]
-            tail[1] -= tail[1] > head[1]
-            visited.add(f"{tail[0]}-{tail[1]}")
-
-        if head[1] == tail[1]:
-            direction = 1 * (tail[0] < head[0]) + (-1) * (tail[0] > head[0])
-            for i in range(tail[0], head[0], direction):
-                visited.add(f"{tail[0]}-{tail[1]}")
-                tail[0] += direction
-            tail[0] -= direction
-
-        if head[0] == tail[0]:
-            direction = 1 * (tail[1] < head[1]) + (-1) * (tail[1] > head[1])
-            for i in range(tail[1], head[1], direction):
-                visited.add(f"{tail[0]}-{tail[1]}")
-                tail[1] += direction
-            tail[1] -= direction
-
+        for step in range(int(value)):
+            head[0] += ((move == "U") - (move == "D"))
+            head[1] += ((move == "R") - (move == "L"))
+            tail = move_tail_closer(head, tail)
+            visited.add(f"{tail[0]}/{tail[1]}")
     return len(visited)
 
 
-print(f"Part 1: {part1()}")
+def part2():
+    with open("input.txt") as file:
+        moves = [i.split(" ") for i in file.read().split('\n')]
+    rope = [[0, 0] for i in range(10)]
+    visited = set()
+    for move, value in moves:
+        for step in range(int(value)):
+            rope[0][0] += ((move == "U") - (move == "D"))
+            rope[0][1] += ((move == "R") - (move == "L"))
+            for knot_index in range(1, len(rope)):
+                rope[knot_index] = move_tail_closer(rope[knot_index-1], rope[knot_index])
+            visited.add(f"{rope[-1][0]}/{rope[-1][1]}")
+    return len(visited)
+
+
+print(f"Part 1: {part1()}\nPart 2: {part2()}")
